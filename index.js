ece2bb5dbf7b5b6fcfd65c9d6a3930199905cd99
Vinn Nduri <ndurivo@gmail.com>
2022-06-16 20:49:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+
+const main = document.getElementById('main');
+main.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+
+newHeader.innerHTML = 'Vinn is the champion'
+
